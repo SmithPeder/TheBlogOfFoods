3c7bea8e3f5924800d4aafb42f992a1cc091a7cf
Peder Smith <smith.peder@gmail.com>
2018-02-07 01:06:08 +0100

    Init commit / project setup

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from '../styles.css';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <h1 className="headerTitle">The Food Blog</h1>
+      </div>
+    );
+  }
+}
