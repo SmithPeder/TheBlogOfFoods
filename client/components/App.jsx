024aae3a9aa8299134cd6bc1cfa0f16b89c3dd4c
Peder Smith <smith.peder@gmail.com>
2018-02-09 21:43:21 +0100

    Add autocreate cards with .json

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,35 +1,29 @@
 import React from 'react';
 import style from '../styles.css';
 import Container from './Container.jsx';
+import data from './content.js';
 
 export default class App extends React.Component {
   render() {
+    let cards = data.cards.map(function(cards) {
+      return (
+        <Container
+          title={cards.title}
+          date={cards.date}
+          source={cards.source}
+        />
+      );
+    });
+
     return (
       <div>
         <div className="header">
-          <h1 className="headerTitle">The Food Blog</h1>
+          <h1 className="headerTitle">The Blog</h1>
+          <div className="menuButton">
+            <img src="client/components/menu.png" />
+          </div>
         </div>
-        <div className="main">
-          <Container title="Test">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Container>
-
-          <Container title="Test">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Container>
-        </div>
+        <div className="main">{cards}</div>
       </div>
     );
   }
--- client/components/Container.jsx
+++ client/components/Container.jsx
@@ -6,8 +6,9 @@ export default class Container<Props> extends React.Component {
     return (
       <div className="container">
         <div className="containerHeader">
-          {this.props.title}
-          <img src="../food.png" width="35px" height="35px" />
+          <span className="date">{this.props.date}</span>
+          <span>{this.props.title}</span>
+          <img src={this.props.source} width="35px" height="35px" />
         </div>
         {this.props.children}
       </div>
