bac50f1be57d9e72c64f94ba0b1b614915482b13
Peder Smith <smith.peder@gmail.com>
2018-03-25 19:26:54 +0200

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './client/index.html',
-  filename: 'index.html',
-  inject: 'body'
+  template: "./client/index.html",
+  filename: "index.html",
+  inject: "body"
 });
 
 module.exports = {
-  entry: './client/index.js',
-  devtool: 'cheap-module-eval-source-map',
+  entry: "./client/index.js",
+  devtool: "cheap-module-eval-source-map",
   output: {
-    path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+    path: path.resolve("dist"),
+    filename: "index_bundle.js"
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, loader: 'style-loader!css-loader' }
+      { test: /\.jsx?$/, loader: "babel-loader", exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: "babel-loader", exclude: /node_modules/ },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   },
   plugins: [HtmlWebpackPluginConfig],
   node: {
-    fs: 'empty'
+    fs: "empty"
   }
 };
